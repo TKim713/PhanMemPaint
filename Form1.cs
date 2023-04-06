1763fefcb005d14c42fbb370c5dc9f24fcf6ecd5
TKim <21110521@student.hcmute.edu.vn>
2023-04-06 16:44:02 +0700

    Thêm thư viện

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,16 @@
-﻿namespace PhanMemPaint
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Net;
+
+namespace PhanMemPaint
 {
     public partial class Form1 : Form
     {
